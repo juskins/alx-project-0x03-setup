90c4011b73e6e4523741e1100d133a379ab28022
juskins <omojuwababatunde1@gmail.com>
2025-11-23 22:27:07 +0100

    Move your interfaces to the right directory

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,12 @@
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+
+
+export interface ButtonProps {
+   buttonLabel: string
+   buttonSize?: string
+   buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+   action?: () => void
+}
